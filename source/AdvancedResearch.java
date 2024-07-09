ea869a768761df8255e718c5e14a2410f4bb4574
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-07-09 10:44:44 +0200

    [change] Separated the menu of advanced research from the menu class

--- source/AdvancedResearch.java
+++ source/AdvancedResearch.java
@@ -1,5 +1,8 @@
 package source;
 import java.util.List;
+import java.util.Scanner;
+
+import static source.Menu.studentDAO;
 
 public class AdvancedResearch {
 
@@ -66,6 +69,54 @@ public class AdvancedResearch {
 
         if(studentsFound.isEmpty()) {
             System.out.println("No students found with the subject: " + subject);
+        }
+    }
+
+    public static void advancedSearchMenu(Scanner scanner){
+        System.out.println("********************************************");
+        System.out.println("*          Advanced Search                  *");
+        System.out.println("********************************************");
+        System.out.println("1. Search by first name");
+        System.out.println("2. Search by last name");
+        System.out.println("3. Search by email");
+        System.out.println("4. Search by age");
+        System.out.println("5. Search by grade");
+        System.out.println("6. Search by subject");
+        System.out.print("Enter your choice: ");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.print("Enter first name: ");
+                String firstName = scanner.next();
+                AdvancedResearch.searchByFirstName(firstName, studentDAO.getAllStudents());
+                break;
+            case 2:
+                System.out.print("Enter last name: ");
+                String lastName = scanner.next();
+                AdvancedResearch.searchByLastName(lastName, studentDAO.getAllStudents());
+                break;
+            case 3:
+                System.out.print("Enter email: ");
+                String email = scanner.next();
+                AdvancedResearch.searchByEmail(email, studentDAO.getAllStudents());
+                break;
+            case 4:
+                System.out.print("Enter age: ");
+                int age = scanner.nextInt();
+                AdvancedResearch.searchByAge(age, studentDAO.getAllStudents());
+                break;
+            case 5:
+                System.out.print("Enter grade: ");
+                double grade = scanner.nextDouble();
+                AdvancedResearch.searchByGrade(grade, studentDAO.getAllStudents());
+                break;
+            case 6:
+                System.out.print("Enter subject: ");
+                String subject = scanner.next();
+                AdvancedResearch.searchBySubject(subject, studentDAO.getAllStudents());
+                break;
+            default:
+                System.out.println("Invalid choice");
         }
     }
 }

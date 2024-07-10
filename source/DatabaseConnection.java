d50a34aa649e8a365eeb91d03955de212f88dac5
Abakar-Issa-Ali <abakar-issa-ali@laplateforme.io>
2024-07-10 13:04:21 +0200

    JDBC problem fixed

--- source/DatabaseConnection.java
+++ source/DatabaseConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/laplateformeTracker";
-    private static final String USERNAME = "";
-    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/laplateformetracker";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Aliabakar";
 
     public static Connection getConnection() {
         Connection connection = null;

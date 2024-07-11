cf32df201a9baabd2438125eab0a1e0fb1d73443
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-07-11 16:03:12 +0200

    [change] Added the handling of the login button

--- LaplateformeTracker/demo/src/main/java/com/example/controllers/AdminLoginController.java
+++ LaplateformeTracker/demo/src/main/java/com/example/controllers/AdminLoginController.java
@@ -1,11 +1,15 @@
 package com.example.controllers;
 
+import com.example.demo.AdminDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,14 +17,34 @@ import java.io.IOException;
 public class AdminLoginController {
 
     @FXML
-    private void handleCreateAdminButtonAction(ActionEvent event) {
-        // Add your create admin logic here
+    private TextField emailField;
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private void handleLoginAdminButtonAction(ActionEvent event) {
+        String email = emailField.getText();
+        String password = passwordField.getText();
+
+        if (AdminDAO.authenticateAdmin(email, password)) {
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("/com/example/demo/main-menu.fxml"));
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                stage.setScene(new Scene(root, 1200, 800));
+                stage.setTitle("Main Menu");
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            showAlert("Login Failed", "Invalid email or password.");
+        }
     }
 
     @FXML
     private void handleBackButtonAction(ActionEvent event) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/com/example/demo/main-menu.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/com/example/demo/landing-page.fxml"));
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root, 1200, 800));
             stage.setTitle("Main Menu");
@@ -28,5 +52,12 @@ public class AdminLoginController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 }

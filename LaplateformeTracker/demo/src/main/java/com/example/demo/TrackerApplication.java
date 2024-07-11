7e71836956727032e6481a9bf449033578cc5c64
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-07-11 16:05:21 +0200

    [change] Rename the main menu to landing page

--- LaplateformeTracker/demo/src/main/java/com/example/demo/TrackerApplication.java
+++ LaplateformeTracker/demo/src/main/java/com/example/demo/TrackerApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class TrackerApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TrackerApplication.class.getResource("main-menu.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TrackerApplication.class.getResource("landing-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
         stage.setTitle("Tracker Application");
         stage.setScene(scene);

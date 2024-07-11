c2941194d7d8c83d99a8dffea5642bf93c0bfc2b
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-07-11 13:58:30 +0200

    [create] Class to connect to the database

---
+++ LaplateformeTracker/demo/src/main/java/com/example/demo/DatabaseConnection.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/laplateformeTracker";
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connected to the database");
+        } catch (SQLException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}

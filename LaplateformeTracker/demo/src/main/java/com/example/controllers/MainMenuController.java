4b8e31a12d332c6c9fafce3b86046f59528e74b3
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-07-11 16:05:41 +0200

    [change] Rename the main menu to landing page

--- LaplateformeTracker/demo/src/main/java/com/example/controllers/MainMenuController.java
+++ LaplateformeTracker/demo/src/main/java/com/example/controllers/MainMenuController.java
@@ -12,19 +12,30 @@ import javafx.scene.Parent;
 import java.io.IOException;
 
 public class MainMenuController {
-    @FXML
-    private Button createAdmin;
-    @FXML
-    private Button loginAdmin;
-    @FXML
-    private Button loginStudent;
-    @FXML
-    private Button exit;
 
     @FXML
-    private void handleCreateAdminButtonAction(ActionEvent event) {
+    private Button createStudentAccount;
+    @FXML
+    private Button deleteStudentAccount;
+    @FXML
+    private Button addGrades;
+    @FXML
+    private Button studentList;
+    @FXML
+    private Button sortStudent;
+    @FXML
+    private Button advancedSearch;
+    @FXML
+    private Button statistics;
+    @FXML
+    private Button importExportData;
+    @FXML
+    private Button Logout;
+
+    @FXML
+    private void handleStudentAccountCreationButtonAction(ActionEvent event){
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/com/example/demo/admin-account-creation.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/com/example/demo/student-account-creation.fxml"));
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root, 1200, 800));
             stage.setTitle("Admin Account Creation");
@@ -32,37 +43,5 @@ public class MainMenuController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    @FXML
-    private void handelLoginAdminButtonAction(ActionEvent event) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/com/example/demofx/admin-login.fxml"));
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(root, 1200, 800));
-            stage.setTitle("Admin Login");
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @FXML
-    private void handleLoginStudentButtonAction(ActionEvent event) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/com/example/demofx/student-login.fxml"));
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(root, 1200, 800));
-            stage.setTitle("Student Login");
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @FXML
-    private void handleExitButtonAction(ActionEvent event) {
-        Stage stage = (Stage) exit.getScene().getWindow();
-        stage.close();
     }
 }

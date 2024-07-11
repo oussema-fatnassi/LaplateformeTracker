05d0a8680c4028f5cd832345806ddb9f640796f8
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-07-11 16:03:41 +0200

    [change] Rename the landing page from main menu

---
+++ LaplateformeTracker/demo/src/main/java/com/example/controllers/LandingPageController.java
@@ -0,0 +1,68 @@
+package com.example.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
+public class LandingPageController {
+    @FXML
+    private Button createAdmin;
+    @FXML
+    private Button loginAdmin;
+    @FXML
+    private Button loginStudent;
+    @FXML
+    private Button exit;
+
+    @FXML
+    private void handleCreateAdminButtonAction(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/example/demo/admin-account-creation.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root, 1200, 800));
+            stage.setTitle("Admin Account Creation");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handelLoginAdminButtonAction(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/example/demo/admin-login.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root, 1200, 800));
+            stage.setTitle("Admin Login");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleLoginStudentButtonAction(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/example/demo/student-login.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root, 1200, 800));
+            stage.setTitle("Student Login");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleExitButtonAction(ActionEvent event) {
+        Stage stage = (Stage) exit.getScene().getWindow();
+        stage.close();
+    }
+}

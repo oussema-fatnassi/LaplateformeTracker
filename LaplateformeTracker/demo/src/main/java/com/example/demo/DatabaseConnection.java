6bffc4cc9cd8952d5201cb38762b472a7e257f58
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-07-15 14:44:40 +0200

    [change] Fixed error with double popup message after operation is finished

--- LaplateformeTracker/demo/src/main/java/com/example/demo/DatabaseConnection.java
+++ LaplateformeTracker/demo/src/main/java/com/example/demo/DatabaseConnection.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/laplateformeTracker";
-    private static final String USERNAME = "";
-    private static final String PASSWORD = "";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "O*9GU9A9";
 
     public static Connection getConnection() {
         Connection connection = null;

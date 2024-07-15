004071d7ac1433a6c3d079692f642fceecccf49f
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-07-15 13:34:17 +0200

    [create] Added util class to split students in age group

---
+++ LaplateformeTracker/demo/src/main/java/com/example/demo/AgeUtils.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+public class AgeUtils {
+
+    public static String calculateAgeCategory(int age) {
+        if (age >= 18 && age <= 25) {
+            return "18-25";
+        } else if (age >= 26 && age <= 30) {
+            return "26-30";
+        } else if (age >= 31 && age <= 35) {
+            return "31-35";
+        } else if (age >= 36 && age <= 40) {
+            return "36-40";
+        } else if (age >= 41 && age <= 45) {
+            return "41-45";
+        } else if (age >= 46 && age <= 50) {
+            return "46-50";
+        } else if (age >= 51 && age <= 60) {
+            return "51-60";
+        } else {
+            return "Unknown";
+        }
+    }
+
+}
